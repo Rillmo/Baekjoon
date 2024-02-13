fca6ea75cd10624d933b222c3f2c85b9ac5516eb
Kirby <maxman306@gmail.com>
2024-02-13 23:42:21 +0900

    solved

--- p_18185/main.cpp
+++ p_18185/main.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main() {
 	int n;
-	int cost;
+	int b, c;
+	unsigned long long cost;
+	int tmp;
 	int	*list;
 	int	j;
 
 	cin >> n;
+	cin >> b; cin >> c;
 	list = new int[n];
 	for (int i=0; i<n; i++) {
 		cin >> list[i];
@@ -15,19 +18,34 @@ int main() {
 	j = 0;
 	cost = 0;
 	while (j < n) {
-		while (list[j] > 0) {
-			if (j < n - 2 && list[j] <= list[j+ 1] && list[j + 1] <= list[j + 2]) {
-				list[j]--; list[j + 1]--; list[j + 2]--;
-				cost += 7;
+		if (j < n - 2 && list[j] <= list[j+ 1] && list[j + 1] <= list[j + 2] && \
+			b + 2*c <= 3 * b) {
+			tmp = list[j];
+			list[j] -= tmp;
+			list[j + 1] -= tmp;
+			list[j + 2] -= tmp;
+			cost += (b + 2 * c) *tmp;
+		}
+		else if (j < n - 1 && list[j] <= list[j + 1] && b + c <= 2 * b) {
+			if (j < n - 2) {
+				tmp = list[j + 1] - list[j + 2];
+				if (list[j] - tmp < 0)
+					tmp = list[j];
 			}
-			else if (j < n - 1 && list[j] <= list[j + 1]) {
-				list[j]--; list[j + 1]--;
-				cost += 5;
+			else
+				tmp = list[j];
+			list[j] -= tmp;
+			list[j + 1] -= tmp;
+			cost += (b + c) * tmp;
+		}
+		else {
+			if (j < n - 1) {
+				tmp = list[j] - list[j + 1];
 			}
-			else {
-				list[j]--;
-				cost += 3;
-			}
+			else
+				tmp = list[j];
+			list[j] -= tmp;
+			cost += b * tmp;
 		}
 		j++;
 	}
