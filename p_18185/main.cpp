97d69e6545ae68ff01894003350719c229846269
Kirby <maxman306@gmail.com>
2024-02-14 21:22:53 +0900

    solved

--- p_18185/main.cpp
+++ p_18185/main.cpp
@@ -12,42 +12,47 @@ int main() {
 	cin >> n;
 	cin >> b; cin >> c;
 	list = new int[n];
-	for (int i=0; i<n; i++) {
+	for (int i=0; i<n; i++)
 		cin >> list[i];
-	}
 	j = 0;
 	cost = 0;
 	while (j < n) {
-		if (j < n - 2 && list[j] <= list[j+ 1] && list[j + 1] <= list[j + 2] && \
-			b + 2*c <= 3 * b) {
-			tmp = list[j];
-			list[j] -= tmp;
-			list[j + 1] -= tmp;
-			list[j + 2] -= tmp;
-			cost += (b + 2 * c) *tmp;
-		}
-		else if (j < n - 1 && list[j] <= list[j + 1] && b + c <= 2 * b) {
-			if (j < n - 2) {
-				tmp = list[j + 1] - list[j + 2];
-				if (list[j] - tmp < 0)
+		while (list[j] > 0) {
+			// cout << "cost : " << cost << " + ";
+			if (j < n - 2 && list[j] <= list[j+ 1] && list[j + 1] <= list[j + 2] && \
+				b + 2*c <= 3 * b) {
+				tmp = list[j];
+				list[j] -= tmp;
+				list[j + 1] -= tmp;
+				list[j + 2] -= tmp;
+				cost += (b + 2 * c) *tmp;
+				// cout << (b + 2 * c) * tmp << " (3)" << endl;
+			}
+			else if (j < n - 1 && list[j] <= list[j + 1] && b + c <= 2 * b) {
+				if (j < n - 2 && list[j] > list[j + 1] - list[j + 2])
+					tmp = list[j + 1] - list[j + 2];
+				else
 					tmp = list[j];
+				list[j] -= tmp;
+				list[j + 1] -= tmp;
+				cost += (b + c) * tmp;
+				// cout << (b + c) * tmp << " (2)" << endl;
 			}
-			else
-				tmp = list[j];
-			list[j] -= tmp;
-			list[j + 1] -= tmp;
-			cost += (b + c) * tmp;
-		}
-		else {
-			if (j < n - 1) {
-				tmp = list[j] - list[j + 1];
+			else {
+				if (j < n - 1 && list[j] > list[j + 1])
+					tmp = list[j] - list[j + 1];
+				else
+					tmp = list[j];
+				list[j] -= tmp;
+				cost += b * tmp;
+				// cout << b * tmp << " (1)" << endl;
 			}
-			else
-				tmp = list[j];
-			list[j] -= tmp;
-			cost += b * tmp;
+			// for (int i=0; i<n; i++) {
+			// 	cout << list[i] << " ";
+			// }
 		}
-		j++;
+			j++;
 	}
+	// cout << endl;
 	cout << cost << endl;
 }
